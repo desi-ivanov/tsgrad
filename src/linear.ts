a2efa5001d23c1704c116f2f72d01f25574a042b
evolveyourmind <20824840+desi-ivanov@users.noreply.github.com>
2022-09-12 23:03:30 +0200

    Refactor

--- src/linear.ts
+++ src/linear.ts
@@ -1,24 +1,38 @@
 import { Parameter } from "./autograd";
 import { Model } from "./model";
+import { reduceSum } from "./util";
+
+class Perceptron extends Model {
+  w: Parameter[];
+  b: Parameter;
+  use_bias: boolean;
+  constructor(input_size: number, use_bias = true, initializer = () => Math.random() * 2 - 1) {
+    super();
+    this.w = Array.from({ length: input_size }, () => new Parameter(initializer()));
+    this.b = new Parameter(initializer());
+    this.use_bias = use_bias 
+  }
+  parameters = () => this.w.concat(this.use_bias ? [this.b] : []);
+  forward = (x: Parameter[]): Parameter[] => {
+    if(x.length != this.w.length) throw new Error(`Input missmatch Expected ${this.w.length} channels, but got ${x.length}`);
+    const r = reduceSum(x.map((v, i) => v.mul(this.w[i])))
+    return this.use_bias ? [r.add(this.b)] : [r];
+  }
+}
 
 export class Linear extends Model {
-  w: Parameter[][];
+  w: Perceptron[];
   b: Parameter | undefined;
   in_channels: number;
   constructor(in_channels: number, out_channels: number, use_bias = true, initializer = () => Math.random() * 2 - 1) {
     super();
     this.in_channels = in_channels
-    this.w = Array.from({ length: out_channels }, () => Array.from({ length: in_channels }, () => new Parameter(initializer())));
-    if(use_bias) {
-      this.b = new Parameter(initializer());
-    }
+    this.w = Array.from({ length: out_channels }, () => new Perceptron(in_channels, use_bias, initializer));
   }
-  parameters = () => this.w.flat().concat(this.b ? [this.b] : []);
+  parameters = () => this.w.flatMap(p => p.parameters());
   forward = (x: Parameter[]): Parameter[] => {
     if(x.length != this.in_channels) throw new Error(`Input missmatch Expected ${this.in_channels} channels, but got ${x.length}`);
-  
-    const y = this.w.map(ws => ws.map((w, i) => w.mul(x[i])).reduce((a, v) => a.add(v)).add(this.b ? this.b : 0));
-
+    const y = this.w.map(p => p.forward(x)[0]);
     if(y.length != this.w.length) throw new Error(`Output missmatch. Expected ${this.w.length} outputs, but got ${y.length}`);
     return y;
   }
